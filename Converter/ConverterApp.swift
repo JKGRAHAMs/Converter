6699e98d91e4a8e506a7516feb7bf8a65e1b8dba
Joey Graham <82058059+JKGRAHAMs@users.noreply.github.com>
2022-01-16 20:44:01 -0600

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Joey Graham on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
